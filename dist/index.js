f3923342c43065f5b0c97095a31fa584e91fac7e
CraftyDragon678 <craftydragon678@gmail.com>
2020-06-15 13:20:02 +0900

    feat: add action.yml file
    chore: update compiled file

--- dist/index.js
+++ dist/index.js
@@ -18,26 +18,60 @@ const dotenv_1 = require("dotenv");
 const rest_1 = require("@octokit/rest");
 dotenv_1.config({ path: path_1.resolve(__dirname, "../.env") });
 const BASE_URL = "https://api.dimigo.in";
-function getYYYYMMDD(date) {
+var Meal;
+(function (Meal) {
+    Meal["breakfast"] = "\uC544\uCE68\uD83C\uDF05";
+    Meal["lunch"] = "\uC810\uC2EC\uD83C\uDF1E";
+    Meal["dinner"] = "\uC800\uB141\uD83C\uDF03";
+})(Meal || (Meal = {}));
+function getYYYYMMDD(date = new Date()) {
     let mm = date.getMonth() + 1;
     let dd = date.getDate();
     return `${date.getFullYear()}${mm < 10 ? "0" + mm : mm}${dd < 10 ? "0" + dd : dd}`;
 }
-function isTomorrow(date) {
+function isTomorrow(date = new Date()) {
     return date.getHours() > 19;
 }
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    const today = new Date();
-    const getNextDay = isTomorrow(today);
-    if (getNextDay) {
-        today.setDate(today.getDate() + 1);
+function getMeal(date = new Date()) {
+    const time = date.getHours() * 100 + date.getMinutes();
+    if (816 <= time && time <= 1340)
+        return Meal.lunch;
+    else if (time <= 1920)
+        return Meal.dinner;
+    return Meal.breakfast;
+}
+function seperateLine(str, width = 35, char = "/") {
+    const arr = str.split(char);
+    let result = "";
+    let start = 0;
+    for (let i = 0; i < 4; ++i) {
+        for (let end = start; end <= arr.length; ++end) {
+            const currentLine = arr.slice(start, end).join(char);
+            if (currentLine.length > width || end === arr.length) {
+                start = end;
+                result += currentLine;
+                break;
+            }
+        }
+        result += "\n";
     }
-    const fetched = yield node_fetch_1.default(`${BASE_URL}/dimibobs/${getYYYYMMDD(today)}`);
+    return result.trim();
+}
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const datetime = new Date();
+    const getNextDay = isTomorrow(datetime);
+    if (getNextDay) {
+        datetime.setDate(datetime.getDate() + 1);
+    }
+    const fetched = yield node_fetch_1.default(`${BASE_URL}/dimibobs/${getYYYYMMDD(datetime)}`);
     const data = (yield fetched.json());
-    const output = `${getNextDay ? "내일의 밥" : "오늘의 밥"}\n` +
-        `아침🌅 ${data.breakfast}\n` +
-        `점심🌞 ${data.lunch}\n` +
-        `저녁🌃 ${data.dinner}`;
+    const meal = getMeal(datetime);
+    const content = seperateLine(meal === Meal.breakfast
+        ? data.breakfast
+        : meal === Meal.lunch
+            ? data.lunch
+            : data.dinner);
+    const title = `${getNextDay ? "내일의 밥" : "오늘의 밥"} - ${meal}`;
     const octokit = new rest_1.Octokit({ auth: `token ${process.env.GH_TOKEN}` });
     const gist = yield octokit.gists
         .get({ gist_id: process.env.GIST_ID })
@@ -45,13 +79,15 @@ function isTomorrow(date) {
     if (!gist)
         return;
     const filename = Object.keys(gist.data.files)[0];
-    yield octokit.gists.update({
+    yield octokit.gists
+        .update({
         gist_id: process.env.GIST_ID,
         files: {
             [filename]: {
-                filename: "dimibob",
-                content: output,
+                filename: title,
+                content: content,
             },
         },
-    });
+    })
+        .catch((e) => console.error("can't update gist"));
 }))();
